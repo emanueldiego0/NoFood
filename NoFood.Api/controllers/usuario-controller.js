be9887ed5c9d9ecd5fd8a8522e4f81fd9b5c0de7
Emanuel Ferreira <emanueldiego0@gmail.com>
2018-11-28 23:43:42 -0200

    quarto commit

--- NoFood.Api/controllers/usuario-controller.js
+++ NoFood.Api/controllers/usuario-controller.js
@@ -1,30 +1,60 @@
 'use strict'
 const repository = require('../repositories/usuario-repository');
+const validation = require('../bin/helpers/validation');
+const md5 = require('md5');
 const _repo = new repository();
+const ctrlBase = require('../bin/base/controller-base');
+
 //criando classe. Poderia ser class
 function usuarioController(){
 
 }
 
 usuarioController.prototype.post = async(req, res) => { 
-    let resultado = await _repo.create(req.body); 
-    res.status(201).send(resultado);
+    let _validationContract = new validation();
+    
+    _validationContract.isRequired(req.body.nome, 'informe o nome');
+    _validationContract.isRequired(req.body.email, 'informe seu email');
+    _validationContract.isEmail(req.body.email, 'email invalido');
+    _validationContract.isRequired(req.body.senha, 'senha é requerida');
+    _validationContract.isRequired(req.body.senhaConfirmacao, 'repeticao da senha é requerida');
+    _validationContract.isTrue(req.body.senha != req.body.senhaConfirmacao, 'as senhas devem ser iguais');
+    
+    let usuarioIsEmailExiste = await _repo.IsEmailExistente(req.body.email);
+    if(usuarioIsEmailExiste){
+        _validationContract.isTrue((usuarioIsEmailExiste.nome != undefined), `Ja existe o email ${req.body.email} cadastrado em nossa base de dados.`);
+    }
+    req.body.senha = md5(req.body.senha);
+
+    ctrlBase.post(_repo, _validationContract, req, res);
 };
 usuarioController.prototype.put = async(req, res) => { 
-    let resultado = await _repo.update(req.params.id, req.body)
-    res.status(202).send(resultado);
+    let _validationContract = new validation();
+    
+    _validationContract.isRequired(req.body.nome, 'informe o nome');
+    _validationContract.isRequired(req.body.email, 'informe seu email');
+    _validationContract.isEmail(req.body.email, 'email invalido');
+    _validationContract.isRequired(req.body.id, 'informe o id do usuario');
+
+    let usuarioIsEmailExiste = await _repo.IsEmailExistente(req.body.email);
+    if(usuarioIsEmailExiste){
+        _validationContract.isTrue(
+            (usuarioIsEmailExiste._id != req.body.id) && 
+            (usuarioIsEmailExiste.nome != undefined), 
+            `Ja existe o email ${req.body.email} cadastrado em nossa base de dados.`);
+    }
+    
+    ctrlBase.put(_repo, _validationContract, req, res);
 };
+
 usuarioController.prototype.get = async(req, res) => {
-    let resultado = await _repo.getAll();
-    res.status(200).send(resultado);
+    ctrlBase.get(_repo, req, res);
 };
 usuarioController.prototype.getById = async(req, res) => {
-    let resultado = await _repo.getById(req.params.id);
-    res.status(200).send(resultado);
+    ctrlBase.getById(_repo, req, res);
 };
 usuarioController.prototype.delete = async(req, res) => {
-    let resultado = await _repo.delete(req.params.id);
-    res.status(204).send(resultado);
+    ctrlBase.delete(_repo, req, res);
 };
 
 module.exports = usuarioController;
